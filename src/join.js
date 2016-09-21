eeeccfaff0f4abee4e8cc16035b4c2bed22c892b
Shakalee14 <shakalee14@gmail.com>
2016-09-20 17:45:36 -0700

    finished join and it was difficult but fun

---
+++ src/join.js
@@ -0,0 +1,43 @@
+const join = ( value, separator ) => {
+
+  var newString = ''
+  var newSeparator = separator
+
+  if( value instanceof Array){
+    if ( separator === undefined) {
+      newSeparator = ','
+    } 
+      for( var index = 0; index < value.length-1; index++){
+      newString += value[index] + newSeparator  
+    }
+    newString += value[index]
+    //newString += value[index++]
+    return newString
+  }
+
+  else if( value.constructor.toString().indexOf("String") > -1){
+
+    for( var index = 0; index < value.length-1; index++){
+      newString += value.charAt(index) + separator  
+    }
+    newString += value.charAt(index)
+    return newString
+  }
+
+  else {
+    return ''
+  }
+
+  // else{
+  //   return newString
+  // }
+
+
+  // return newString
+
+}
+
+// var A = ['a', 'b', 'c']
+// array = A + ""
+
+export { join }
