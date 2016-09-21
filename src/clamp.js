2168c8fd9a06b51bf93eb2e0294ed7a967ca79cc
anasauce <anasauceda1843@gmail.com>
2016-09-21 15:28:43 -0700

    WIP clamp

--- src/clamp.js
+++ src/clamp.js
@@ -2,24 +2,30 @@ const clamp = (...args) => {
    const { value, lowerBound, upperBound } = parseClampParams( args )
    if( Array.isArray(value) ) {
      return NaN
-   }
-   else if( (typeof value === 'number' || typeof value === 'boolean') && (typeof lowerBound === 'number' || typeof lowerBound === 'boolean') && (typeof upperBound === 'number' || typeof upperBound === 'boolean')) {
-      value = Number(value)
-      lowerBound = Number(lowerBound)
-      upperBound = Number(upperBound)
-      if( value <= lowerBound ) {
+   } else if ( value === null || lowerBound === null || upperBound === null ) {
+      return 0
+    } else if ( typeof value === 'string' ) {
+     return NaN
+   } else if( typeof value === 'number' || 'boolean' ) {
+      if( +value <= +lowerBound ) {
         return lowerBound
-      } else if (value >= upperBound) {
+      } else if (+value >= +upperBound) {
         return upperBound
       } else {
-        return value
+        return +value
       }
-   } else {
+   } 
+   else {
       return NaN
    }
 }
 
 const parseClampParams = args => {
+  for(let index = 0; index < args.length; index++ ) {
+    if(args[index] === null) {
+      return { value: null, lowerBound: null, upperBound: null }
+    }
+  } 
   if( args.length === 1 ) {
     return { value: args[0], lowerBound: args[0], upperBound: args[0] }
   } else if( args.length === 2 ) {
